76c66ab3e5567f1a78466448362251f8bca4700f
Austin <austinz@mac.com>
2019-03-28 21:26:32 -0700

    optimising elliptic curve math

--- src/secp256k1.js
+++ src/secp256k1.js
@@ -1,7 +1,21 @@
 const bigInt = require('big-integer')
 const elliptic = require('simple-js-ec-math')
+const fs = require('fs')
 const ModPoint = elliptic.ModPoint
 const Curve = elliptic.Curve
+
+let preprocessing = {}
+let previousPoint
+const points = JSON.parse(fs.readFileSync('./secp256k1-preprocessing.json'))
+const first = ModPoint.fromJSON(points[0])
+preprocessing[first.toString()] = {}
+for (let point of points) {
+  point = ModPoint.fromJSON(point)
+  if (previousPoint) {
+    preprocessing[first.toString()][previousPoint.toString()] = point
+  }
+  previousPoint = point
+}
 
 const g = new ModPoint(
   bigInt('79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798', 16),
@@ -13,5 +27,6 @@ const secp256k1 = new Curve(
   bigInt('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141', 16),
   bigInt('2').pow('256').minus(bigInt('2').pow('32')).minus('977'),
   g,
+  preprocessing
 )
 module.exports = secp256k1
